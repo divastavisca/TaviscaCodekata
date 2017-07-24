283e09381da22eeae01f409c209f7de87424e551
divastavisca <divasag93@gmail.com>
2017-07-24 23:42:59 +0530

    Initialized git repo

---
+++ MarsRover/MarsRover/Mars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Mars
+    {
+        private int _length { get; set; }
+        private int _breadth { get; set; }
+
+        public Mars(int length,int breadth)
+        {
+            _length = length;
+            _breadth = breadth;
+        }
+
+      
+    }
+}
---
+++ MarsRover/MarsRover/Rover.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Rover
+    {
+        private int _x_position { get; set; }
+        private int _y_position { get; set; }
+
+
+    }
+}
